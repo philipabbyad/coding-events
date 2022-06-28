93af40a94b935943ea9b86d3c5117135121529bc
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-06-28 12:03:18 -0600

    Added form handler method. Modified event index template to handle empty list.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,6 +3,7 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,22 +14,27 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     // path is /events
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
         model.addAttribute("events", events);
         return "events/index";
     }
 
     // path is /events/create
     @GetMapping("create")
-    public String renderCreateEVentForm() {
+    public String renderCreateEventForm() {
         return "events/create";
+    }
+
+    // path is /events/create (Ok to share path as above because they handle different types of requests
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        // Non-specified redirect redirects user to root path of controller, which is /events in this case
+        return "redirect:";
     }
 
 }
