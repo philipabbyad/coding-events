2597f914df0f687dff892c0b55eff0639396fbd4
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-06-28 16:53:46 -0600

    Events now a HashMap to allow for name and description. Allows user to create event, Name is required and default description displayed if left blank.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,17 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    //private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap<>();
 
     // path is /events
     @GetMapping
     public String displayAllEvents(Model model) {
+        events.put("Agile2022", "Agile Conference in Nashville, TN");
+        events.put("UberConf 2022", "Java conference in Denver, CO");
+        events.put("Hugo Conf", "Goland conference Online");
         model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
@@ -33,8 +38,8 @@ public class EventController {
 
     // path is /events/create (Ok to share path as above because they handle different types of requests)
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(eventName);
+    public String processCreateEventForm(@RequestParam String eventName, String eventDescription) {
+        events.put(eventName, eventDescription);
         // Non-specified redirect redirects user to root path of controller, which is /events in this case
         return "redirect:";
     }
