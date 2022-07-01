4db20efa6861e64c6e1ca9a017ca325eb27e5c77
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-07-01 13:43:24 -0600

    Added unique ID to Event model.

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -2,12 +2,21 @@ package org.launchcode.codingevents.models;
 
 public class Event {
 
+    private int id;
+    private static int nextId = 1;
+
     private String name;
     private String description;
 
     public Event(String name, String description) {
         this.name = name;
         this.description = description;
+        this.id = nextId;
+        nextId++;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
