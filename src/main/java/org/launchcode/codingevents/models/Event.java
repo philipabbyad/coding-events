46fc9a6feb6e2cca0596c5ec46d6c3cd92e77ac6
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-07-04 17:03:02 -0600

    Added Entity annotation to Event model.

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,10 +1,12 @@
 package org.launchcode.codingevents.models;
 
+import javax.persistence.Entity;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Objects;
 
+@Entity
 public class Event {
 
     private int id;
