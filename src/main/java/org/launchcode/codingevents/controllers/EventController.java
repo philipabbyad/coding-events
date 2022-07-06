355864b0c8e4af3c9160905b7d7753e7b1a2c7b6
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-07-06 17:39:38 -0600

    Added functionality to display tags for events.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -103,6 +103,7 @@ public class EventController {
             Event event = result.get();
             model.addAttribute("title", event.getName() + " Details");
             model.addAttribute("event", event);
+            model.addAttribute("tags", event.getTags());
         }
 
         return "events/detail";
