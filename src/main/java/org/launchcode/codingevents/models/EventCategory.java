8829969f26b2c20881400f015934a9ab35d9195f
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-07-05 20:34:20 -0600

    Added relationship annotation and field to event category class.

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -2,13 +2,21 @@ package org.launchcode.codingevents.models;
 
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class EventCategory extends AbstractEntity{
 
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
     private String name;
+
+    // Mapped By value has to be the name of the field in the class we're storing that creates the relationship
+    @OneToMany(mappedBy = "eventCategory")
+    //List itself cannot change, contents still can
+    private final List<Event> events = new ArrayList<>();
 
     public EventCategory(@Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.") String name) {
         this.name = name;
@@ -22,6 +30,10 @@ public class EventCategory extends AbstractEntity{
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Event> getEvents() {
+        return events;
     }
 
     @Override
